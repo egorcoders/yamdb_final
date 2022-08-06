498c543b2c66ced5dc7601281a977627c62461e3
Egor <egorcoders@gmail.com>
2022-08-06 12:09:10 +0500

    added flake8 3

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from reviews.models import Category, Genre, Review, Title
-
 from api.filters import TitleFilter
 from api.mixins import MixinViewSet
 from api.permissions import (IsAdminOrReadOnly,
@@ -11,6 +9,7 @@ from api.permissions import (IsAdminOrReadOnly,
 from api.serializers import (CategorySerializer, CommentSerializer,
                              GenreSerializer, ReviewSerializer,
                              TitleViewSerializer, TitleWriteSerializer)
+from reviews.models import Category, Genre, Review, Title
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
