eb1f00f7a79ad3599cf0043c8601e201b655890c
Egor <egorcoders@gmail.com>
2022-08-06 11:27:05 +0500

    added settings

--- api_yamdb/api_yamdb/delete.py
+++
@@ -1,4 +0,0 @@
-from smtplib import SMTPSenderRefused
-
-
-SMTPSenderRefused
---
+++ api_yamdb/api_yamdb/vs_code_settings.py
@@ -0,0 +1,86 @@
+{
+  "workbench.colorTheme": "Community Material Theme Darker",
+  "workbench.editorAssociations": {
+    "*.ipynb": "jupyter-notebook",
+    "*.sqlite3": "default"
+  },
+  "notebook.cellToolbarLocation": {
+    "default": "right",
+    "jupyter-notebook": "left"
+  },
+  "python.analysis.typeCheckingMode": "strict",
+  "security.workspace.trust.untrustedFiles": "open",
+  "git.autofetch": true,
+  "editor.formatOnSaveMode": "file",
+  "python.linting.mypyEnabled": true,
+  "python.linting.pylintEnabled": true,
+  "python.linting.enabled": true,
+  "terminal.integrated.lineHeight": 1.2,
+  "editor.fontSize": 14,
+  "editor.formatOnSave": true,
+  "editor.codeActionsOnSave": {
+    "source.organizeImports": true
+  },
+  "editor.minimap.enabled": true,
+  "editor.renderWhitespace": "none",
+  "git.enableSmartCommit": true,
+  "explorer.confirmDragAndDrop": false,
+  "workbench.colorCustomizations": {},
+  "workbench.iconTheme": "material-icon-theme",
+  "prettier.singleQuote": true,
+  "html.format.enable": false,
+  "html.format.indentHandlebars": true,
+  "emmet.triggerExpansionOnTab": true,
+  "files.associations": {
+    "*html": "html",
+    "*.html": "django-html"
+  },
+  "editor.linkedEditing": true,
+  "git.confirmSync": false,
+  "mssql.connections": [
+    {
+      "server": "{{put-server-name-here}}",
+      "database": "{{put-database-name-here}}",
+      "user": "{{put-username-here}}",
+      "password": "{{put-password-here}}"
+    }
+  ],
+  "debug.javascript.autoAttachFilter": "always",
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "explorer.confirmDelete": false,
+  "editor.wordWrap": "on",
+  "[python]": {
+    "editor.defaultFormatter": "ms-python.python"
+  },
+  "editor.unicodeHighlight.allowedCharacters": {
+    "о": true,
+    "а": true,
+    "е": true,
+    "р": true,
+    "с": true,
+    "б": true,
+    "З": true,
+    "г": true,
+    "Т": true,
+    "у": true,
+    "х": true,
+    "С": true,
+    "К": true,
+    "У": true,
+    "А": true,
+    "О": true,
+    "В": true
+  },
+  "bracket-pair-colorizer-2.depreciation-notice": false,
+  "workbench.startupEditor": "none",
+  "update.mode": "none",
+  "extensions.autoCheckUpdates": false,
+  "extensions.autoUpdate": false,
+  "update.showReleaseNotes": false,
+  "terminal.integrated.enableMultiLinePasteWarning": false,
+  "typescript.tsserver.maxTsServerMemory": 8072,
+  "files.maxMemoryForLargeFilesMB": 7096,
+  "window.zoomLevel": 1,
+  "python.defaultInterpreterPath": "/opt/local/bin/python3.9",
+  "tabnine.experimentalAutoImports": true
+}
